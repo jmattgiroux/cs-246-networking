149679afc1c89cc93f30e2084e8bb4ae57f6c965
Jmattgiroux <git config --global user.email jmattgiroux@gmail.comgit config --global user.email>
2020-12-16 21:10:51 -0600

    Our -very- complex translator is working beautifully now; now to put this thing on a server so the world may enjoy it

--- project/main.py
+++ project/main.py
@@ -1,17 +1,41 @@
+import random
+
 
 class Translator:
+    dogWord = ["BORK", "baRK", "ArF", "ruff"]
 
+    # https://www.w3schools.com/python/python_lists.asp
     def __init__(self):
         self.stringToTranslate = " "
+        self.translatedString = ""
+        self.randomNumber = 1
+
 
     def translate_to_dog(self):
-        pass
+        # Reset our translation
+        self.translatedString = ""
+        # https://www.w3schools.com/python/python_for_loops.asp
+        # https://www.geeksforgeeks.org/python-string-count/
+        for x in range(self.stringToTranslate.count(" ") + 1):
+            self.translatedString = self.translatedString + self.get_dog_word(self) + " "
 
     def set_string(self, string):
         self.stringToTranslate = string
 
     def get_string(self):
         return self.stringToTranslate
+
+    def get_dog_string(self):
+        self.translate_to_dog(self)
+        return self.translatedString
+
+    def update_number(self):
+        # https://www.geeksforgeeks.org/random-numbers-in-python/
+        self.randomNumber = random.choice([0, 1, 2, 3])
+
+    def get_dog_word(self):
+        self.update_number(self)
+        return self.dogWord[self.randomNumber]
 
 
 if __name__ == '__main__':
@@ -21,5 +45,5 @@ if __name__ == '__main__':
     # https://www.askpython.com/python/examples/python-user-input
     translator.set_string(translator, input("Please enter the text you would like to translate to dog-ish: "))
 
-    print(translator.get_string(translator))
+    print(translator.get_dog_string(translator))
 
