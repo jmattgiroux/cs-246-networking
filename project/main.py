908236abbe1dbde062622d8d8081e90aac621073
Jmattgiroux <git config --global user.email jmattgiroux@gmail.comgit config --global user.email>
2020-12-16 20:32:28 -0600

    created functions for translator class; will be getting translator to work before getting the socket stuff working for this project

---
+++ project/main.py
@@ -0,0 +1,19 @@
+
+class Translator:
+
+    def __init__(self):
+        self.stringToTranslate = " "
+
+    def translate_to_dog(self):
+        pass
+
+    def update_string(self, string):
+        pass
+
+
+
+
+
+if __name__ == '__main__':
+    print("Test")
+
