8a3fb07d032a123589443643f4f716e422d3841e
Jmattgiroux <git config --global user.email jmattgiroux@gmail.comgit config --global user.email>
2020-12-16 20:40:16 -0600

    now takes user input; now to get the 'translator' working

--- project/main.py
+++ project/main.py
@@ -7,13 +7,19 @@ class Translator:
     def translate_to_dog(self):
         pass
 
-    def update_string(self, string):
-        pass
+    def set_string(self, string):
+        self.stringToTranslate = string
 
-
-
+    def get_string(self):
+        return self.stringToTranslate
 
 
 if __name__ == '__main__':
-    print("Test")
+
+    translator = Translator
+
+    # https://www.askpython.com/python/examples/python-user-input
+    translator.set_string(translator, input("Please enter the text you would like to translate to dog-ish: "))
+
+    print(translator.get_string(translator))
 
